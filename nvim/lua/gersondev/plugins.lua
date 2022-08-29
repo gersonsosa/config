757e9416abe6e3cdc587803c74e8b7b4ef922da7
Gerson Sosa <gersonsosa@gmail.com>
2022-08-29 09:29:58 +0200

    feat, install packer if not installed, adds new plugins
    - terminal management
    - comment shortcuts
    - key binding helper

--- nvim/lua/gersondev/plugins.lua
+++ nvim/lua/gersondev/plugins.lua
@@ -1,13 +1,60 @@
-return require('packer').startup(function()
+local fn = vim.fn
+
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  }
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  max_jobs = 50,
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+    prompt_border = "rounded", -- Border style of prompt popups.
+  },
+}
+
+return packer.startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- Keybindings
+  use "folke/which-key.nvim"
+
+  -- Telescope
   use {
     'nvim-telescope/telescope.nvim',
     requires = {
       { 'nvim-lua/plenary.nvim' }
     }
   }
+
+  use { 'nvim-telescope/telescope-ui-select.nvim' }
 
   use 'neovim/nvim-lspconfig'
 
@@ -36,6 +83,9 @@ return require('packer').startup(function()
     requires = { 'kyazdani42/nvim-web-devicons' }
   }
 
+  use { "akinsho/toggleterm.nvim", tag = 'v2.*', config = function()
+    require("toggleterm").setup()
+  end }
   -- tpope plugins because this deserves a special section
   use 'tpope/vim-eunuch'
   use 'tpope/vim-surround'
@@ -47,14 +97,20 @@ return require('packer').startup(function()
   use 'airblade/vim-gitgutter'
   use 'mattn/gist-vim'
   use 'nathanaelkane/vim-indent-guides'
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
 
   use 'rizzatti/dash.vim'
 
   use 'fatih/vim-go'
   use 'mfussenegger/nvim-jdtls'
 
+  -- Coloschemes
   use { 'dracula/vim', as = 'dracula' }
-
   use 'folke/tokyonight.nvim'
 
 end)
