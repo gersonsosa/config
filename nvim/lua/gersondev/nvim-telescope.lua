3958f414b172cde92c234c4fc95f7d87cce0227b
Gerson Sosa <4366859+gersonsosa@users.noreply.github.com>
2022-09-29 16:14:32 +0200

    feat, add mappings to telescope for metals

--- nvim/lua/gersondev/nvim-telescope.lua
+++ nvim/lua/gersondev/nvim-telescope.lua
@@ -1,1 +1,12 @@
-require('telescope').setup()
+vim.keymap.set("n", "<leader>mc", [[<cmd>lua require("telescope").extensions.metals.commands()<CR>]],
+  { desc = 'Show metals commands', noremap = true })
+
+local actions = require("telescope.actions")
+require('telescope').setup {
+  prompt_prefix = "❯",
+  mappings = {
+    n = {
+      ["f"] = actions.send_to_qflist,
+    },
+  },
+}
