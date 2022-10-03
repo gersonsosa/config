73665f805ac2c29103cbdabbe05aeb06093250b9
Gerson Sosa <4366859+gersonsosa@users.noreply.github.com>
2022-10-03 22:08:07 +0200

    feat, polishes lualine config

--- nvim/lua/gersondev/lualine-config.lua
+++ nvim/lua/gersondev/lualine-config.lua
@@ -6,21 +6,21 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
-    disabled_filetypes = {},
+    component_separators = '',
+    section_separators = { left = ' ', right = ' ' },
+    disabled_filetypes = { 'qf' },
     always_divide_middle = true,
   },
   sections = {
     lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
+      { 'mode', separator = { left = '█', right = ' ' }, right_padding = 2 },
     },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
     lualine_c = { 'filename' },
     lualine_x = { 'g:metals_status' },
     lualine_y = { obsession_status, 'filetype', 'progress' },
     lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
+      { 'location', separator = { left = ' ', right = '█' }, left_padding = 2 },
     },
   },
   inactive_sections = {
