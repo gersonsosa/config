faf30fe7019d004767eaf60faf797887d63f38f1
Gerson Sosa <4366859+gersonsosa@users.noreply.github.com>
2022-10-17 10:05:50 +0200

    chore, updates cmp lsp config removes deprecated call

--- nvim/lua/gersondev/nvim-lsp.lua
+++ nvim/lua/gersondev/nvim-lsp.lua
@@ -80,7 +80,7 @@ nvim_lsp.sumneko_lua.setup {
 
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
