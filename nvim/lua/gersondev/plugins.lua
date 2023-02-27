0063c04541f785a161f521345c239704dfa3f06f
Gerson Sosa <4366859+gersonsosa@users.noreply.github.com>
2023-02-27 17:55:47 +0100

    refactor, make neo tree lazy and update

--- nvim/lua/gersondev/plugins.lua
+++ nvim/lua/gersondev/plugins.lua
@@ -23,6 +23,7 @@ local plugins = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
     },
+    cmd = { "NeoTreeFocusToggle", "NeoTreeRevealToggle" },
     config = function()
       vim.g.neo_tree_remove_legacy_commands = 1
       require("neo-tree").setup({
