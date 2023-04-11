72e5d1d100c0020f6b0d5f044c94b4ac070f6e71
Gerson Sosa <4366859+gersonsosa@users.noreply.github.com>
2023-04-11 11:02:49 +0200

    feat, adds telescope search to neotree

--- nvim/lua/gersondev/neo-tree.lua
+++ nvim/lua/gersondev/neo-tree.lua
@@ -1,4 +1,33 @@
 vim.g.neo_tree_remove_legacy_commands = 1
+
+local function build_telescope_mappings(state)
+  return function(bufnr, _)
+    local actions = require "telescope.actions"
+    actions.select_default:replace(function()
+      actions.close(bufnr)
+      local action_state = require "telescope.actions.state"
+      local selection = action_state.get_selected_entry()
+      local filename = selection.filename
+      if (filename == nil) then
+        filename = selection[1]
+      end
+      -- any way to open the file without triggering auto-close event of neo-tree?
+      require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
+    end)
+    return true
+  end
+end
+
+local function build_telescope_options(state)
+  local node = state.tree:get_node()
+  local path = node:get_id()
+  return {
+    cwd = path,
+    search_dirs = { path },
+    attach_mappings = build_telescope_mappings(state)
+  }
+end
+
 require("neo-tree").setup({
   window = {
     mappings = {
@@ -16,7 +45,23 @@ require("neo-tree").setup({
         local renderer = require('neo-tree.ui.renderer')
         renderer.focus_node(state, siblings[1]:get_id())
       end,
-      ["z"] = "none"
+      ["z"] = "none",
     }
-  }
+  },
+  filesystem = {
+    window = {
+      mappings = {
+        ["tf"] = "telescope_find",
+        ["tg"] = "telescope_grep",
+      },
+    },
+    commands = {
+      telescope_find = function(state)
+        require('telescope.builtin').find_files(build_telescope_options(state))
+      end,
+      telescope_grep = function(state)
+        require('telescope.builtin').live_grep(build_telescope_options(state))
+      end,
+    },
+  },
 })
