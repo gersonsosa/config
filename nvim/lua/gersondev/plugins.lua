df616ad78b4239b0f03b34801e681f03cd56afc2
Gerson Sosa <4366859+gersonsosa@users.noreply.github.com>
2023-12-11 21:21:02 +0100

    chore: enables copilot in fish files

--- nvim/lua/gersondev/plugins.lua
+++ nvim/lua/gersondev/plugins.lua
@@ -249,7 +249,7 @@ local plugins = {
     "github/copilot.vim",
     config = function()
       -- Disable copilot by default, invoke with M+\
-      local file_types = { rust = true, lua = true }
+      local file_types = { rust = true, lua = true, fish = true }
       file_types["*"] = false
       vim.inspect(file_types)
       vim.g.copilot_filetypes = file_types
