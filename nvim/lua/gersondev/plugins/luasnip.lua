5a9e4bf23203950ac650ed6dfd10ea5222a900be
Gerson Sosa <4366859+gersonsosa@users.noreply.github.com>
2024-08-14 07:12:53 +0200

    refactor: separate snippets per language with luasnip

--- nvim/lua/gersondev/plugins/luasnip.lua
+++ nvim/lua/gersondev/plugins/luasnip.lua
@@ -1,6 +1,5 @@
 return {
   "L3MON4D3/LuaSnip",
-  -- follow latest release.
   version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
   -- install jsregexp (optional!).
   build = "make install_jsregexp",
@@ -12,26 +11,58 @@ return {
     local i = ls.insert_node
     local fmt = require("luasnip.extras.fmt").fmt
 
-    ls.add_snippets("all", {
-      s("def", fmt("def {func_name}({params}):{body}", {
-        func_name = i(1, "F"),
-        params = i(2, "params..."),
-        body = i(0, "..."),
-      })),
-      s("iff", fmt("if {cond} then {body} end", {
-        cond = i(1, "cond"),
-        body = i(0, "..."),
-      })),
+    ls.add_snippets("scala", {
+      s(
+        "def",
+        fmt("def {func_name}({params}):{body}", {
+          func_name = i(1, "F"),
+          params = i(2, "params..."),
+          body = i(0, "..."),
+        })
+      ),
     })
 
-    vim.keymap.set({ "i" }, "<C-l>", function() ls.expand() end, { silent = true })
-    vim.keymap.set({ "i", "s" }, "<C-k>", function() ls.jump(1) end, { silent = true })
-    vim.keymap.set({ "i", "s" }, "<C-j>", function() ls.jump(-1) end, { silent = true })
+    ls.add_snippets("go", {
+      s(
+        "ife",
+        fmt(
+          [[
+            if {err} != nil {{
+              fmt.Errorf("{message} %w", {err})
+            }}
+          ]],
+          {
+            err = i(1, "err"),
+            message = i(2, "message..."),
+          }
+        )
+      ),
+    })
 
-    vim.keymap.set({ "i", "s" }, "<C-?>", function()
+    ls.add_snippets("lua", {
+      s(
+        "iff",
+        fmt("if {cond} then {body} end", {
+          cond = i(1, "cond"),
+          body = i(0, "..."),
+        })
+      ),
+    })
+
+    vim.keymap.set({ "i" }, "<C-l>", function()
+      ls.expand()
+    end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<C-k>", function()
+      ls.jump(1)
+    end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<C-j>", function()
+      ls.jump(-1)
+    end, { silent = true })
+
+    vim.keymap.set({ "i", "s" }, "<C-x>", function()
       if ls.choice_active() then
         ls.change_choice(1)
       end
     end, { silent = true })
-  end
+  end,
 }
